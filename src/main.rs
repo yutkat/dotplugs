6248163d43f84df299f7b6b62024c919c7341f56
Yuta Katayama <yutakatay@gmail.com>
2019-10-19 23:08:14 +0900

    Add json output mode

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,12 @@ use failure::format_err;
 fn main() -> Result<(), Error> {
     let matches = args::load()?;
     match matches.subcommand() {
-        ("check", _) => { dotplugs::check()? },
+        ("check", Some(sub_m)) => {
+            if sub_m.is_present("json") {
+                return dotplugs::check_output_json();
+            }
+            dotplugs::check()?
+        },
         _ => { return Err(format_err!("subcommand not found")); },
     }
     Ok(())
