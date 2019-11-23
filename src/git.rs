38d499c83587af95045e153624ad25da3bb62d87
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-11-23 17:47:36 +0900

    Fix fetch because git2.rs does not support shallow fetch

--- src/git.rs
+++ src/git.rs
@@ -2,18 +2,19 @@ use crate::repository::Repositories;
 use crate::repository::Repository;
 use failure::format_err;
 use failure::Error;
-use log::{debug, warn};
-use serde::{Deserialize, Serialize};
-use std::sync::{Arc, Mutex};
 use futures::executor;
 use futures::task::SpawnExt;
+use log::{debug, warn};
+use serde::{Deserialize, Serialize};
 use std::io::Write;
+use std::sync::{Arc, Mutex};
 use termion::clear;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub enum UpdateStatus {
     Required,
     Already,
+    NotGitRepository,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -34,9 +35,9 @@ fn get_status_sync(repos: &Repositories) -> Result<Vec<GitStatus>, Error> {
     for repo in repos.clone() {
         eprint!("\r{}Checking: {}", clear::CurrentLine, repo.uri);
         std::io::stdout().flush().unwrap();
-        match get_repository_status(&repo) {
+        match fetch_repository(&repo) {
             Ok(sts) => git_statuses.push(sts),
-            Err(e) => warn!("{:?}", e),
+            Err(e) => warn!("\n{:?}: {:?}", &repo, e),
         }
     }
     eprint!("\r{}", clear::CurrentLine);
@@ -47,34 +48,65 @@ fn get_status_async(repos: &Repositories) -> Result<Vec<GitStatus>, Error> {
     let git_statuses = Arc::new(Mutex::new(Vec::<GitStatus>::new()));
     let pool = executor::ThreadPool::new()?;
     let mut futures = vec![];
-
     for repo in repos.clone() {
         let git_statuses = Arc::clone(&git_statuses);
         let future = async move {
             eprint!("\r{}Checking: {}", clear::CurrentLine, repo.uri);
             std::io::stdout().flush().unwrap();
-            match get_repository_status(&repo) {
+            match fetch_repository(&repo) {
                 Ok(sts) => git_statuses.lock().unwrap().push(sts),
-                Err(e) => warn!("{:?}", e),
+                Err(e) => {
+                    warn!("\r{}", clear::CurrentLine);
+                    warn!("{:?}: {:?}", &repo, e);
+                }
             }
         };
         futures.push(pool.spawn_with_handle(future)?);
     }
     executor::block_on(futures::future::join_all(futures));
-    let g = Arc::try_unwrap(git_statuses)
-        .map_err(|e| format_err!("Async Error {:?}", e))?;
+    let g = Arc::try_unwrap(git_statuses).map_err(|e| format_err!("Async Error {:?}", e))?;
     eprint!("\r{}", clear::CurrentLine);
     Ok(g.into_inner()?)
 }
 
-fn get_repository_status(repo: &Repository) -> Result<GitStatus, Error> {
-    let git_repo = git2::Repository::open(&repo.dir)?;
-    git_repo.find_remote("origin")?.fetch(&[""], None, None)?;
+fn fetch_repository(repo: &Repository) -> Result<GitStatus, Error> {
+    let git_repo = match git2::Repository::open(&repo.dir) {
+        Ok(x) => x,
+        Err(_) => {
+            return Ok(GitStatus {
+                uri: repo.uri.to_string(),
+                dir: repo.dir.to_string(),
+                status: UpdateStatus::NotGitRepository,
+            })
+        }
+    };
+    // libgit2 does not implement shallow fetch
+    // https://github.com/libgit2/libgit2/issues/3058
+    fetch_repository_by_command(repo)?;
+
+    // let branch = get_current_branch(&git_repo)?;
+    // git_repo.find_remote("origin")?.fetch(
+    //     &[&branch],
+    //     Some(&mut git2::FetchOptions::new()),
+    //     None,
+    // )?;
+
     Ok(GitStatus {
         uri: repo.uri.to_string(),
         dir: repo.dir.to_string(),
         status: get_update_status(&git_repo)?,
     })
+}
+
+fn fetch_repository_by_command(repo: &Repository) -> Result<(), Error> {
+    let output = std::process::Command::new("git")
+        .args(&["fetch", "-a"])
+        .current_dir(&repo.dir)
+        .output()?;
+    if !output.status.success() {
+        warn!("git fetch is failure [Exit code: {}]", output.status);
+    }
+    Ok(())
 }
 
 fn get_update_status(repo: &git2::Repository) -> Result<UpdateStatus, Error> {
@@ -83,6 +115,7 @@ fn get_update_status(repo: &git2::Repository) -> Result<UpdateStatus, Error> {
 
     let local_hash = repo.revparse_single("HEAD")?.id();
     let remote_hash = repo.revparse_single(&remote_branch_name)?.id();
+    // let remote_hash = repo.revparse_single("FETCH_HEAD")?.id();
     debug!(
         "local_hash: {:?} remote_hash: {:?}",
         &local_hash, &remote_hash
@@ -110,9 +143,9 @@ mod tests {
     extern crate pretty_env_logger;
 
     fn init() {
-        std::env::set_var("RUST_LOG", "debug");
         let _ = pretty_env_logger::formatted_builder()
             .is_test(true)
+            .parse_filters("DEBUG")
             .try_init();
     }
 
@@ -174,6 +207,31 @@ mod tests {
             .unwrap();
         let n = get_update_status(&git_repo)?;
         assert_eq!(n, UpdateStatus::Already);
+        Ok(())
+    }
+
+    #[test]
+    fn get_repository_status_ok() -> Result<(), Error> {
+        let repo = Repository {
+            uri: "https://github.com/fatih/vim-go.git".to_string(),
+            dir: "/home/osft/dotfiles/.vim/plugged/vim-go".to_string(),
+        };
+        fetch_repository(&repo)?;
+        Ok(())
+    }
+
+    #[test]
+    fn fetch_test() -> Result<(), Error> {
+        init();
+        let repo = Repository {
+            uri: "https://github.com/Shougo/defx.nvim.git".to_string(),
+            dir: "/home/osft/dotfiles/.vim/plugged/defx.nvim".to_string(),
+        };
+        let git_repo = git2::Repository::open(&repo.dir)?;
+        let branch = get_current_branch(&git_repo)?;
+        git_repo
+            .find_remote("origin")?
+            .fetch(&[&branch], None, None)?;
         Ok(())
     }
 }
