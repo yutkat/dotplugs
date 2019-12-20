92317f37a2ab9a6095324859b909f4ab1e9e5009
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-12-21 03:03:39 +0900

    Fix a bug that could not be created all zsh plugin list due to lazy evaluation

--- src/repository/zplugin.rs
+++ src/repository/zplugin.rs
@@ -1,6 +1,6 @@
 use crate::repository::CanReposit;
+use crate::repository::GitDirectory;
 use crate::repository::Repositories;
-use crate::repository::Repository;
 use failure::format_err;
 use failure::Error;
 use std::process::Command;
@@ -9,41 +9,13 @@ pub struct Zplugin;
 
 impl CanReposit for Zplugin {
     fn get_repositories() -> Result<Repositories, Error> {
-        if !Zplugin::exists_plugin_manager()? {
-            return Ok(vec![]);
-        }
-        let dir = Zplugin::get_plugin_dir()?;
-        let short_urls = Zplugin::get_url()?;
-        Zplugin::create_repositories(&short_urls, &dir)
+        let path = Zplugin::get_plugin_root_dir()?;
+        GitDirectory::get_repositories(path)
     }
 }
 
 impl Zplugin {
-    fn exists_plugin_manager() -> Result<bool, Error> {
-        let cmd = format!(r##"source ~/.zshrc && command -v zplugin > /dev/null 2>&1"##);
-        log::debug!("exists check: {}", cmd);
-        let status = Command::new("zsh").arg("-c").arg(cmd).status()?;
-        log::debug!("process exited with: {}", status);
-        Ok(status.success())
-    }
-
-    fn get_url() -> Result<Vec<String>, Error> {
-        let cmd = format!(
-            r##"source ~/.zshrc && zplugin list | cut -d' ' -f1 | sed 's/\x1b\[[0-9;]*m//g'"##
-        );
-        log::debug!("output zplugin list: {}", cmd);
-        let output = Command::new("zsh").arg("-c").arg(cmd).output()?;
-        log::debug!("process exited with: {}", output.status);
-        let stdout = output.stdout;
-        let urls = String::from_utf8(stdout)?;
-        let urls_v = urls
-            .split_ascii_whitespace()
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>();
-        Ok(urls_v)
-    }
-
-    fn get_plugin_dir() -> Result<String, Error> {
+    fn get_plugin_root_dir() -> Result<String, Error> {
         let cmd = format!(r##"source ~/.zshrc && zplugin zstatus | grep 'Plugin directory' | cut -d' ' -f3 | tr -d '\n' | sed 's/\x1b\[[0-9;]*m//g'"##);
         log::debug!("zplugin cmd: {}", cmd);
         let output = Command::new("zsh").arg("-c").arg(cmd).output()?;
@@ -55,39 +27,5 @@ impl Zplugin {
             return Err(format_err!("zplugin dir not found {}", &dir));
         }
         Ok(dir)
-    }
-
-    fn create_repositories<S: AsRef<str>, T: AsRef<str>>(
-        urls: &Vec<S>,
-        dir: &T,
-    ) -> Result<Repositories, Error> {
-        let mut r = vec![];
-        for u in urls {
-            let repo = Repository {
-                uri: format!("https://git::@github.com/{}", u.as_ref()),
-                dir: format!("{}/{}", dir.as_ref(), u.as_ref().replace("/", "---")),
-            };
-            r.push(repo);
-        }
-        Ok(r)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn init() {
-        let _ = pretty_env_logger::formatted_builder()
-            .is_test(true)
-            .parse_filters("DEBUG")
-            .try_init();
-    }
-
-    #[test]
-    fn get_plugin_dir_normal() -> Result<(), Error> {
-        init();
-        Zplugin::get_plugin_dir()?;
-        Ok(())
     }
 }
