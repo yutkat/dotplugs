394d51f86d5d7a8db116450f2d42cb7d1762f3c9
Yuta Katayama <yutakatay@gmail.com>
2019-12-21 03:03:39 +0900

    Fix a bug that could not be created all zsh plugin list due to lazy evaluation

--- src/repository/tpm.rs
+++ src/repository/tpm.rs
@@ -1,9 +1,7 @@
 use crate::repository::CanReposit;
+use crate::repository::GitDirectory;
 use crate::repository::Repositories;
-use crate::repository::Repository;
-use failure::format_err;
 use failure::Error;
-use std::path::Path;
 use std::process::Command;
 
 pub struct Tpm;
@@ -13,10 +11,11 @@ impl CanReposit for Tpm {
         if !Tpm::is_running_tmux()? {
             return Ok(vec![]);
         }
-        if !Tpm::exists_plugin_manager()? {
-            return Ok(vec![]);
-        }
-        Tpm::create_repositories()
+        let path = match std::env::var("TMUX_PLUGIN_MANAGER_PATH") {
+            Ok(n) => n,
+            Err(_) => return Ok(vec![]),
+        };
+        GitDirectory::get_repositories(path)
     }
 }
 
@@ -31,56 +30,5 @@ impl Tpm {
             .status()?;
         log::debug!("process exited with: {}", status);
         Ok(status.success())
-    }
-
-    fn exists_plugin_manager() -> Result<bool, Error> {
-        let path = match std::env::var("TMUX_PLUGIN_MANAGER_PATH") {
-            Ok(n) => n,
-            Err(_) => return Ok(false),
-        };
-        if path.is_empty() {
-            return Ok(false);
-        }
-        if !std::path::Path::new(&path).exists() {
-            return Ok(false);
-        }
-        log::debug!("tpm exists");
-        Ok(true)
-    }
-
-    fn get_url<P: AsRef<Path>>(path: P) -> Result<String, Error> {
-        let cmd = format!(
-            r##"cd {} && git config --get remote.origin.url"##,
-            path.as_ref().to_str().ok_or(format_err!("convert error"))?
-        );
-        log::debug!("command to get tpm remote origin: {}", cmd);
-        let output = Command::new("sh").arg("-c").arg(cmd).output()?;
-        log::debug!("process exited with: {}", output.status);
-        let stdout = output.stdout;
-        let mut url = String::from_utf8(stdout)?;
-        let len = url.trim_end_matches(&['\r', '\n'][..]).len();
-        url.truncate(len);
-        Ok(url)
-    }
-
-    fn create_repositories() -> Result<Repositories, Error> {
-        let mut r = vec![];
-        let dir = std::env::var("TMUX_PLUGIN_MANAGER_PATH")?;
-        for entry in std::fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                let url = Tpm::get_url(&path)?;
-                let repo = Repository {
-                    uri: url,
-                    dir: path
-                        .to_str()
-                        .ok_or(format_err!("convert error"))?
-                        .to_string(),
-                };
-                r.push(repo);
-            }
-        }
-        Ok(r)
     }
 }
